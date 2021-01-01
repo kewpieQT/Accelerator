dbb3e4ab2fbb6199a6036eaebdb4ce9d7281853d
kewpie <kewpie.qthy@gmail.com>
2021-01-01 23:37:09 +0800

    add hook library

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,14 @@
 #include <jni.h>
 #include <string>
+#include "fbjni/fbjni.h"
+#include "dlopen.h"
+#include "alloctracker.h"
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_kewpie_accelerator_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+jint JNICALL JNI_OnLoad(JavaVM *vm, void *) {
+
+    return facebook::jni::initialize(vm, [] {
+        ndk_init(facebook::jni::Environment::current());
+        hookFunc();
+    });
+
 }
--- app/src/main/java/com/kewpie/accelerator/MainActivity.kt
+++ app/src/main/java/com/kewpie/accelerator/MainActivity.kt
@@ -11,14 +11,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Example of a call to a native method
-        sample_text.text = stringFromJNI()
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    external fun stringFromJNI(): String
 
     companion object {
         // Used to load the 'native-lib' library on application startup.
